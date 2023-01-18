a39cae4f3e5bae6b266da181ccd1c2b4f4a292c1
damkis1 <damiankiszka@o2.pl>
2023-01-18 15:00:58 +0100

    added mainPane.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module mp3player {
     requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
     exports pl.damkis1.mp3player.main to javafx.graphics;
 }
--- src/main/java/pl/damkis1/mp3player/main/Main.java
+++ src/main/java/pl/damkis1/mp3player/main/Main.java
@@ -1,6 +1,9 @@
 package pl.damkis1.mp3player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +12,9 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
+        Pane mainPane = FXMLLoader.load(getClass().getResource("/fxml/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
         stage.setTitle("MP3Player");
         stage.show();
     }
