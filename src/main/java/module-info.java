e68b0ae4c494d8099325c7f65770902fc9661f92
damkis1 <damiankiszka@o2.pl>
2023-01-20 13:32:48 +0100

    added packet controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,5 @@ module mp3player {
     requires javafx.controls;
     requires javafx.fxml;
     exports pl.damkis1.mp3player.main to javafx.graphics;
+    opens pl.damkis1.mp3player.controller to javafx.fxml;
 }
