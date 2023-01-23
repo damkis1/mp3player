7e8e03f8b4777522ec54c0cdab1117218f4376dd
damkis1 <damiankiszka@o2.pl>
2023-01-23 18:15:25 +0100

    added methods configureVolume, configureButtons

--- src/main/java/pl/damkis1/mp3player/controller/MainController.java
+++ src/main/java/pl/damkis1/mp3player/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -35,8 +36,28 @@ public class MainController {
     @FXML
     private Slider volumeSlider;
 
+
     public void initialize(){
         System.out.println("MainController created");
+        configureVolume();
+        configureButtons();
+    }
+
+    private void configureVolume(){
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->{
+            System.out.println("Wciśnięto przysisk na suwaku");
+        });
+    }
+
+    private void configureButtons(){
+        previousButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
+        nextButton.setOnAction(actionEvent -> System.out.println("następna piosenka"));
+        playButton.setOnAction(actionEvent -> {
+            if (playButton.isSelected()){
+                System.out.println("Play");
+            }else System.out.println("Stop");
+        });
+
     }
 
 }
